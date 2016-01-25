99f2506aaec2fb50f3223d878c8e1ab0c7964fb9
Alex Larson and Jason Wuerch <wuerchjason@gmail.com>
2016-01-25 10:44:57 -0800

    passed first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,16 @@
-function Slimeball(firstName, lastName, favoriteColours, age) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.favoriteColours = favoriteColours;
-  this.age = age;
+var tasks = []
+
+function toDoList(task) {
+  this.task = task
 }
 
-Slimeball.prototype.fullName = function() {
-  return  "<span class = 'link'>" + this.firstName + " " + this.lastName + "</span>";
+toDoList.prototype.newTask = function(task) {
+  tasks.push(task)
+  console.log(tasks)
+  return tasks
 }
 
-Slimeball.prototype.isAge = function() {
-  return this.fullName() + " is " + this.age + " years old and their favorite colors are" + this.favoriteColours;
-}
 
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    var input1 = $("#firstNameInput").val();
-    var input2 = $("#lastNameInput").val();
-    var input3 = $("#favoriteColoursInput").val();
-    var input4 = $("#ageInput").val();
-    var newPerson = new Slimeball(input1, input2, input3, input4);
-
-    $(".firstNameSpan").append(newPerson.fullName());
-    $(".lastNameSpan").append(newPerson.isAge());
-    $(".firstNameSpan").click(function () {
-      $(".lastNameSpan").show();
-    })
-    console.log(newPerson.firstName);
-    event.preventDefault();
-
-  });
-
 
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,22 +1,10 @@
 
-describe("addressBook", function () {
-  it("will list first name, last name, favorite colors, and age", function (){
-    var contact1 = new Slimeball("Alex", "Larson", ["blue", "periwinkle"], 18);
-    expect(contact1.firstName).to.equal("Alex");
-    expect(contact1.lastName).to.equal("Larson");
-    expect(contact1.favoriteColours).to.eql(["blue", "periwinkle"]);
-    expect(contact1.age).to.equal(18);
-  });
+describe("toDoList", function () {
+  it("will allow user to add tasks to a list of tasks", function (){
+    var firstItem = new toDoList("Clean")
+    expect(firstItem.task).to.equal("Clean");
+    expect(firstItem.newTask("Clean")).to.eql(["Clean"])
 
-  it("will print out the full name", function () {
-    var contact1 = new Slimeball("John", "Johnson", ["red", "pink"], 19);
-    expect(contact1.fullName()).to.equal("John Johnson");
   });
-
-  it("will print out the full name and age", function () {
-    var contact1 = new Slimeball("Steve", "Stevenson", ["red", "pink"], 100);
-    expect(contact1.isAge()).to.equal("Steve Stevenson is 100 years old");
-  });
-
 
 });
