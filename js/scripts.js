dd503728f5740c617559176004c27a40a44e0ca1
Alex Larson and Jason Wuerch <student@epicodus.com>
2016-01-25 09:52:10 -0800

    Hello, it's me again. Made changes

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,30 @@ function Slimeball(firstName, lastName, favoriteColours, age) {
 }
 
 Slimeball.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+  return  "<span class = 'link'>" + this.firstName + " " + this.lastName + "</span>";
 }
 
 Slimeball.prototype.isAge = function() {
-  return this.fullName() + " is " + this.age + " years old";
+  return this.fullName() + " is " + this.age + " years old and their favorite colors are" + this.favoriteColours;
 }
 
 $(document).ready(function() {
+  $("form").submit(function(event) {
+    var input1 = $("#firstNameInput").val();
+    var input2 = $("#lastNameInput").val();
+    var input3 = $("#favoriteColoursInput").val();
+    var input4 = $("#ageInput").val();
+    var newPerson = new Slimeball(input1, input2, input3, input4);
+
+    $(".firstNameSpan").append(newPerson.fullName());
+    $(".lastNameSpan").append(newPerson.isAge());
+    $(".firstNameSpan").click(function () {
+      $(".lastNameSpan").show();
+    })
+    console.log(newPerson.firstName);
+    event.preventDefault();
+
+  });
 
 
 });
